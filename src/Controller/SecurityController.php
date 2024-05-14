f702319c3c3ba5da81d9b1c7b7ada995cfd84484
cassius269 <fahamygaston@gmail.com>
2024-05-15 00:51:50 +0200

    affichage dynamique du menu et mise en place de du blocage des tentatives de connexions et traductions des messages d'erreur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
@@ -26,6 +27,7 @@ class SecurityController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
             $user->setCreatedAt(new \DateTimeImmutable());
+            $user->setRoles(["role_user"]);
             $entityManager->persist($user);
             $entityManager->flush();
             $this->addFlash('success', 'Vous êtes inscrit(e) maintenant');
@@ -38,18 +40,27 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/connexion', name: 'connexion')]
-    public function connect(AuthenticationUtils $authenticationUtils): Response
+    public function connect(AuthenticationUtils $authenticationUtils, RequestStack $requeststack): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $username = $authenticationUtils->getLastUsername();
+
+        $session = $requeststack->getSession();
+        $session->set('username', $username);
 
 
         if ($error) {
             $this->addFlash("erreur", 'Une erreur s\'est produite');
         }
+
         return $this->render('security/connexion.html.twig', [
             "error" => $error,
             "username" => $username
         ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {
     }
 }
