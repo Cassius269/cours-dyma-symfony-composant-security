a6e14e6aa698be320c9fe4ceddb62ad750059551
Cassius269 <fahamygaston@gmail.com>
2024-05-18 17:03:53 +0200

    création d'une route d'édition de post

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route(path: '/blog', name: 'blog')]
 class BlogController extends AbstractController
@@ -19,26 +19,45 @@ class BlogController extends AbstractController
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(Post $post, EntityManagerInterface $em, Request $request, Security $security): Response
     {
-        //$edit = $post ?? new Post();
-        if (isset($post)) {
-            dd('hello');
-        }
-        //$post = new Post();
+   
+      //  dd($post);
+      //  $user = $security->getUser();
 
-        $user = $security->getUser();
         $form = $this->createForm(PostType::class, $post);
-
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $post->setCreatedAt(new \DateTimeImmutable());
-            $post->setUser($user);
-            $em->persist($post);
-            $em->flush();
-            $this->addFlash('success', 'Votre post a été publié');
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->addFlash('success', 'Votre post a été créé');
+    
+                return $this->redirectToRoute('home');
+            }
+            
+        return $this->render('blog/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
-            return $this->redirectToRoute('home');
-        }
+
+    #[Route('/edit', name: 'createpost')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function createpost( EntityManagerInterface $em, Request $request, Security $security): Response
+    {
+   
+        $post = new Post();
+        $user = $security->getUser();
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $post->setCreatedAt(new \DateTimeImmutable());
+                $post->setUser($user);
+                $em->persist($post);
+                $em->flush();
+                $this->addFlash('success', 'Votre post a été créé');
+    
+                return $this->redirectToRoute('home');
+            }
 
         return $this->render('blog/index.html.twig', [
             'form' => $form->createView()
